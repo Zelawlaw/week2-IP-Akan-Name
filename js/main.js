3a4a0d3d90770570efb297f9791e43b63d96ee13
Lawrence Karani <lawrencekarani@gmail.com>
2022-10-07 17:58:59 +0300

    Added date validation function

--- js/main.js
+++ js/main.js
@@ -13,31 +13,38 @@ let gender;
 let akan_name;
 let button = document.getElementById("submit");
 
-const validateDate = (date)=>{
-
-
-
-}
+const validateDate = (date) => {
+  if (date > new Date()) {
+    alert("Birth Date is in the Future! Kindly enter correct Date of Birth");
+    return false;
+  } else if (date === null) {
+    alert("incorrect Date! Kindly enter correct date");
+    return false;
+  } else {
+    return true;
+  }
+};
 
 button.addEventListener("click", () => {
-  
   thedate = document.getElementById("birthday").valueAsDate;
   gender = document.getElementById("gender").value;
-  let day_of_week =  thedate.getUTCDay();
-  switch(gender){
-  case 'male':
-  akan_name = DAY_NAME_MAP.get(day_of_week)[0];
-  break;
-  case 'female':
-    akan_name = DAY_NAME_MAP.get(day_of_week)[1];
-    break;
+
+  if (!validateDate(thedate)) {
+    return;
+  }
+
+  let day_of_week = thedate.getUTCDay();
+  switch (gender) {
+    case "male":
+      akan_name = DAY_NAME_MAP.get(day_of_week)[0];
+      break;
+    case "female":
+      akan_name = DAY_NAME_MAP.get(day_of_week)[1];
+      break;
     default:
       alert("Kindly choose Gender!");
       break;
-}
-console.log("day of week :"+day_of_week);
-console.log("Akan name is :"+akan_name);
-}
-);
-
-
+  }
+  console.log("day of week :" + day_of_week);
+  console.log("Akan name is :" + akan_name);
+});
